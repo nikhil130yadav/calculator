7048f869f2bfbc1e147e04555a3caaa4b9705f27
nikhil <nikhil.nikhil2008@gmail.com>
2020-04-20 22:00:35 +0530

    ccalulator 2nd

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 import operations
 def display():
-    choice = int(input("1.Add two numbers\n2.Subtract two numbers\n"))
+    choice = int(input("1.Add two numbers\n2.Subtract two numbers\n3.Multlipy"))
     return choice
 
 def operate(choice):
@@ -9,11 +9,15 @@ def operate(choice):
         number_2=input("\Enter second number")
         result = "Sum of "+number_1+'and'+number_2+'is:'+str(operations.add(int(float(number_1)),int(float(number_2))))
         return result
-    else:
+    if choice==2:
             number_1=input("\nEnter First number")
             number_2=input("\Enter second number")
             result = "Sum of"+number_1+'and'+number_2+'is:'+str(operations.subtract(int(float(number_1)),int(float(number_2))))
             return result
+    if choice==3:
+            number_1=input("\nEnter First number")
+            number_2=input("\Enter second number")
+            result = "Sum of"+number_1+'and'+number_2+'is:'+str(operations.multiplication(int(float(number_1)),int(float(number_2))))
 if __name__ == "__main__":
             print("enter your choice")
             choice = display()
